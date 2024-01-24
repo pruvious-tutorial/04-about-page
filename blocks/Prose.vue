49060d028250826e0fb8ca67ca9b0e1fd49c2aff
Muris Ceman <murisceman@gmail.com>
2024-01-24 15:59:46 +0100

    init

---
+++ blocks/Prose.vue
@@ -0,0 +1,20 @@
+<template>
+  <Container class="content-block">
+    <PruviousHTML :html="text" class="max-w-content prose" />
+  </Container>
+</template>
+
+<script lang="ts" setup>
+import { defineBlock, editorField } from '#pruvious'
+
+defineBlock({
+  icon: 'Pencil',
+})
+
+defineProps({
+  text: editorField({
+    required: true,
+    toolbar: ['bold', 'italic', 'link', 'heading2', 'heading3', 'paragraph', 'link', 'bulletList'],
+  }),
+})
+</script>
